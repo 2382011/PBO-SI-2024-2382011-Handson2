14d269041011b2be5887f87137909cde66f20a9f
WIN-11 <2382011@unai.edu>
2024-09-10 10:26:49 +0700

    Handson2

---
+++ TernaryOperator.java
@@ -0,0 +1,9 @@
+public class TernaryOperator {
+    public static void main(String[] args) {
+        int nilailulusMinimum = 70;
+        int nilaiMahasiswa = 60;
+        String hasil = nilaiMahasiswa >= nilailulusMinimum ? "Lulus" : "Tidak Lulus";
+        System.out.println(hasil);
+
+    }
+}
